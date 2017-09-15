5d8847002d4a56dc89e9dc96e618457f8aaab446
qwezxcrvrv <qwezxcrvrv@gmail.com>
2017-09-15 22:00:34 +0000

    v9 User Associations

---
+++ YelpCamp/v8/seeds.js
@@ -0,0 +1,67 @@
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var Comment = require("./models/comment");
+
+var data = [
+    {
+        name: "Cloud's Rest", 
+        image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    },
+    {
+        name: "Desert Mesa", 
+        image: "https://farm4.staticflickr.com/3859/15123592300_6eecab209b.jpg",
+        description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+    },
+    {
+        name: "Canyon Floor", 
+        image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
+        description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+    }
+];
+
+function seedDB(){
+    //Remove all comments
+    Comment.remove({},function(err){
+        if(err){
+            console.log(err);
+        } else{
+            console.log("Comments removed");
+                //Remove all campgrounds
+                Campground.remove({}, function(err){
+                    if(err){
+                        console.log(err);
+                    } else{
+                        console.log("Campground removed");
+                    }
+                    //add a few campgrounds
+                    data.forEach(function(seed){
+                        Campground.create(seed, function(err,campground){
+                            if(err){
+                                console.log(err);
+                            }else{
+                                console.log("A New Campgound added");
+                                //create a comment
+                                Comment.create(
+                                    {
+                                        text: "This place is great, but I wish there was internet",
+                                        author: "Homer"
+                                    },function(err, comment){
+                                        if(err){
+                                            console.log(err);
+                                        } else{
+                                            campground.comments.push(comment);
+                                            campground.save();
+                                            console.log("New comment created");
+                                        }
+                                    });
+                            }
+                        });
+                    });
+                });
+        }
+    });
+
+}
+
+module.exports = seedDB;
